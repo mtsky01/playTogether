68519820413c2f40276ea8b2b05424fe414df917
farseerzzy <farseercarl@gmail.com>
2016-04-21 17:38:16 +0800

    最热活动 最新活动标题
    新添加

--- playTogether/HomeViewController.swift
+++ playTogether/HomeViewController.swift
@@ -8,10 +8,21 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+enum TableViewMode{
+    /// 半糖精选
+    case BanTangGoodSelectMode
+    /// 用户推荐
+    case UserRecommendMode
+}
 
+class HomeViewController: UIViewController ,SegmentViewDelegate {
+
+    private var tableViewMode = TableViewMode.BanTangGoodSelectMode
+    
+    private var segmentView = SegmentView()
     override func viewDidLoad() {
         super.viewDidLoad()
+        buildSegmentView()
 
         // Do any additional setup after loading the view.
     }
@@ -31,5 +42,22 @@ class HomeViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func buildSegmentView(){
+        segmentView = SegmentView.init(frame: CGRectMake(0, 300, SCREEN_WIDTH, 45), firstTitle: "最热活动", secondTitle: "最新活动")
+        segmentView.delegate = self
+        view.addSubview(segmentView)
+    }
+    
+    func clickSegmentView(clickIndex: Int) {
+        if(tableViewMode == .BanTangGoodSelectMode)
+        {
+            tableViewMode = .UserRecommendMode
+        }else
+        {
+            tableViewMode = .BanTangGoodSelectMode
+        }
+        //banTangView.reloadData()
+    }
 
 }
